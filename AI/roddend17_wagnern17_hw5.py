d8d703b9b166feb01c02e6238dc91093dd8024e4
DavidRodden <roddend17@up.edu>
2017-04-10 21:39:36 -0700

    Added more parameters to be used when invoking back_propogation

--- AI/roddend17_wagnern17_hw5.py
+++ AI/roddend17_wagnern17_hw5.py
@@ -40,38 +40,35 @@ class AIPlayer(Player):
         self.output_weights = [0.5] * 17
         self.bias_weights = [0.5] * 17
 
-
-    def back_propogation(self, input_values):
-        target = 1 # Get from fitness function
-        actual = 0.5 # Get from neural net function
+    def back_propogation(self, input_values, hidden_output, target, actual):
         error = target - actual
         delta = actual * (1 - actual) * error
         perc_delta = []
         for i in range(0, len(self.output_weights)):
             perc_error = self.output_weights[i] * delta
-            current_perc_delta = self.hidden_ouput[i] * (1 - self.hidden_output[i]) * perc_error
-            self.output_weights[i] += 0.8 * delta * self.hidden_output[i]
+            current_perc_delta = hidden_output[i] * (1 - hidden_output[i]) * perc_error
+            self.output_weights[i] += 0.8 * delta * hidden_output[i]
             self.bias_weights[i] += 0.8 * current_perc_delta
             perc_delta.append(current_perc_delta)
 
         # first 12 can be iterated through as they are a mapping to itself
-        for i in range(0, len(self.input_values)):
-            self.initial_weights[i] += 0.8 * perc_delta[i] * self.input_values[i]
+        for i in range(0, len(input_values)):
+            self.initial_weights[i] += 0.8 * perc_delta[i] * input_values[i]
 
-        self.initial_weights[12] += 0.8 * perc_delta[0] * self.input_values[12]
-        self.initial_weights[13] += 0.8 * perc_delta[1] * self.input_values[12]
-        self.initial_weights[14] += 0.8 * perc_delta[1] * self.input_values[13]
-        self.initial_weights[15] += 0.8 * perc_delta[2] * self.input_values[13]
-        self.initial_weights[16] += 0.8 * perc_delta[4] * self.input_values[13]
-        self.initial_weights[17] += 0.8 * perc_delta[5] * self.input_values[13]
-        self.initial_weights[18] += 0.8 * perc_delta[3] * self.input_values[14]
-        self.initial_weights[19] += 0.8 * perc_delta[4] * self.input_values[14]
-        self.initial_weights[20] += 0.8 * perc_delta[7] * self.input_values[14]
-        self.initial_weights[21] += 0.8 * perc_delta[8] * self.input_values[15]
-        self.initial_weights[22] += 0.8 * perc_delta[9] * self.input_values[15]
-        self.initial_weights[23] += 0.8 * perc_delta[10] * self.input_values[15]
-        self.initial_weights[24] += 0.8 * perc_delta[0] * self.input_values[16]
-        self.initial_weights[25] += 0.8 * perc_delta[3] * self.input_values[16]
+        self.initial_weights[12] += 0.8 * perc_delta[0] * input_values[12]
+        self.initial_weights[13] += 0.8 * perc_delta[1] * input_values[12]
+        self.initial_weights[14] += 0.8 * perc_delta[1] * input_values[13]
+        self.initial_weights[15] += 0.8 * perc_delta[2] * input_values[13]
+        self.initial_weights[16] += 0.8 * perc_delta[4] * input_values[13]
+        self.initial_weights[17] += 0.8 * perc_delta[5] * input_values[13]
+        self.initial_weights[18] += 0.8 * perc_delta[3] * input_values[14]
+        self.initial_weights[19] += 0.8 * perc_delta[4] * input_values[14]
+        self.initial_weights[20] += 0.8 * perc_delta[7] * input_values[14]
+        self.initial_weights[21] += 0.8 * perc_delta[8] * input_values[15]
+        self.initial_weights[22] += 0.8 * perc_delta[9] * input_values[15]
+        self.initial_weights[23] += 0.8 * perc_delta[10] * input_values[15]
+        self.initial_weights[24] += 0.8 * perc_delta[0] * input_values[16]
+        self.initial_weights[25] += 0.8 * perc_delta[3] * input_values[16]
         return None
 
     """
@@ -82,6 +79,7 @@ class AIPlayer(Player):
     Parameters:
         state - GameState to score.
     """
+
     @staticmethod
     def map_input(state):
 
